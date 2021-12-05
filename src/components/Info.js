46855ed99f5f28a811c8f484511c0f94b539eaf4
adrianmurage <adrianmurage21@gmail.com>
2021-12-05 13:12:53 +0300

    modifies the info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,32 @@
+// email button && linkedin button
+import ProfilePhoto from "../images/profile-pic.jpg";
 function Info() {
-    return(
-        <h1>Info goes here</h1>
-    )
+  return (
+    <header className="Info-header">
+      <img src={ProfilePhoto} alt="profile"></img>
+      <h1 className="Info-name">Adrian Murage</h1>
+      <h2 className="Info-role">Frontend Developer</h2>
+      <a className="Info-website" href="https://adrianmurage.com">
+        adrianmurage.com
+      </a>
+      <div className="Info-buttons">
+      <a
+        className="Info-mailto"
+        href='mailto:adrianmurage21@gmail.com'
+        type="button"
+      >
+        Email
+      </a>
+      <a
+        className="Info-linkedin"
+        href='https://www.linkedin.com/in/adrianmurage/'
+        type="button"
+      >
+        LinkedIn
+      </a>
+      </div>
+    </header>
+  );
 }
 
 export default Info;
