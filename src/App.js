57ee6798f6258f7520ce6eacbd7c16d884ccef1c
adrianmurage <adrianmurage21@gmail.com>
2021-12-05 20:53:20 +0300

    modifies the info component
    adds font awesome icons npm package
    completes info component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Info from "./components/Info";
 import About from "./components/About";
 import Interests from "./components/Interests";
 import Footer from "./components/Footer";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faEnvelope, faCheckSquare);
 
 function App() {
   return (
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,34 @@
+// email button && linkedin button
+import ProfilePhoto from "../images/profile-pic.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Info() {
-    return(
-        <h1>Info goes here</h1>
-    )
+  return (
+    <header className="Info-header">
+      <img src={ProfilePhoto} alt="profile"></img>
+      <h1 className="Info-name">Adrian Murage</h1>
+      <h2 className="Info-role">Frontend Developer</h2>
+      <a className="Info-website" href="https://adrianmurage.com">
+        adrianmurage.com
+      </a>
+      <div className="Info-buttons">
+        <a
+          className="Info-mailto"
+          href="mailto:adrianmurage21@gmail.com"
+          type="button"
+        >
+          <FontAwesomeIcon icon="envelope" /> Email
+        </a>
+        <a
+          className="Info-linkedin"
+          href="https://www.linkedin.com/in/adrianmurage/"
+          type="button"
+        >
+          <FontAwesomeIcon icon={["fab", "linkedin"]} /> LinkedIn
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Info;
