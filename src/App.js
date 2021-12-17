55b7ff01f152e8793c85a6e9de553a7565f0497d
adrianmurage <adrianmurage21@gmail.com>
2021-12-17 13:25:59 +0300

    add wrapper around about and interests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Info from "./components/Info";
-import About from "./components/About";
-import Interests from "./components/Interests";
+import AboutInterestsWrapper from "./components/AboutInterestsWrapper";
 import Footer from "./components/Footer";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -12,8 +11,7 @@ function App() {
   return (
     <div className="App-container">
       <Info />
-      <About />
-      <Interests />
+      <AboutInterestsWrapper />
       <Footer />
     </div>
   );
---
+++ src/components/AboutInterestsWrapper.js
@@ -0,0 +1,15 @@
+import About from "./About"
+import Interests from "./Interests"
+function AboutInterestsWrapper() {
+    return(
+        <>
+        <div className="AboutInterestWrapper">
+            <About />
+            <Interests />
+        </div>
+        </>
+
+    )
+}
+
+export default AboutInterestsWrapper;
