b02ed771ed5b2273932827c760d3fe888be7c8b2
adrianmurage <adrianmurage21@gmail.com>
2021-12-17 14:54:42 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,21 @@ function Footer() {
     <div className="Footer">
       <a
         className="Footer-icon"
-        href="https://www.linkedin.com/in/adrianmurage/"
+        href="https://twitter.com/adrianmurage"
         type="button"
       >
         <FontAwesomeIcon icon={["fab", "twitter-square"]} size="2x"/>
       </a>
       <a
         className="Footer-icon"
-        href="https://www.linkedin.com/in/adrianmurage/"
-        type="button"
-      >
-        <FontAwesomeIcon icon={["fab", "facebook-square"]} size="2x"/>
-      </a>
-      <a
-        className="Footer-icon"
-        href="https://www.linkedin.com/in/adrianmurage/"
+        href="https://www.instagram.com/adrianmurage/"
         type="button"
       >
         <FontAwesomeIcon icon={["fab", "instagram-square"]} size="2x"/>
       </a>
       <a
         className="Footer-icon"
-        href="https://www.linkedin.com/in/adrianmurage/"
+        href="https://github.com/adrianmurage"
         type="button"
       >
         <FontAwesomeIcon icon={["fab", "github-square"]} size="2x"/>
